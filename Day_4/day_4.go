3d81e6b5527c5b32b3cc5fe25bec4e45de54b66d
DN <daniel.neuhoff@googlemail.com>
2023-12-04 23:53:56 +0100

    Day 4 Part 2

--- Day_4/day_4.go
+++ Day_4/day_4.go
@@ -12,6 +12,7 @@ import (
 
 type Card struct {
 	winning, own []int
+	copies       int
 }
 
 func parseForPart1(input []string) []Card {
@@ -19,10 +20,9 @@ func parseForPart1(input []string) []Card {
 
 	for _, line := range input {
 		parsingWinning := true
-		actCard := Card{}
+		actCard := Card{copies: 1}
 		r := regexp.MustCompile(`\||\s+`)
 		splits := r.Split(line, -1)
-		fmt.Println(splits)
 		for _, part := range splits[2:] {
 			if part == "" {
 				parsingWinning = false
@@ -51,27 +51,40 @@ func playPart1(fileName string) int {
 	input := readFile(fileName)
 	cards := parseForPart1(input)
 	result := 0
-	for idx, card := range cards {
+	for _, card := range cards {
 		count := 0
 		for _, winner := range card.winning {
 			if slices.Contains(card.own, winner) {
 				count++
 			}
 		}
-		score := 0
 		if count > 0 {
-			score = int(math.Pow(2, float64(count-1)))
+			result += int(math.Pow(2, float64(count-1)))
 		}
-		result += score
-		fmt.Printf("Card %d, Count: %d Score: %d \n", idx, count, score)
 	}
-
 	return result
 }
 
 func playPart2(fileName string) int {
+	input := readFile(fileName)
+	cards := parseForPart1(input)
+	result := 0
+	for cardNo, card := range cards {
+		count := 0
+		for _, winner := range card.winning {
+			if slices.Contains(card.own, winner) {
+				count++
+			}
+		}
+		for cntCopy := 0; cntCopy < card.copies; cntCopy++ {
+			for cntCard := cardNo + 1; cntCard <= cardNo+count && cntCard < len(cards); cntCard++ {
+				cards[cntCard].copies++
+			}
+		}
+		result += card.copies
+	}
 
-	return 0
+	return result
 }
 
 func main() {
@@ -84,21 +97,21 @@ func main() {
 
 	retVal = playPart1("input.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 22488 {
 		panic("Part 0 failed")
 	}
 	fmt.Println("Part 0 passed")
 
 	retVal = playPart2("test0.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 30 {
 		panic("Test 1 failed")
 	}
 	fmt.Println("Test 1 passed")
 
 	retVal = playPart2("input.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 7013204 {
 		panic("Part 1 failed")
 	}
 	fmt.Println("Part 1 passed")
