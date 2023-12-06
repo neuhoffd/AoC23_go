a4612e01e3fc701a2f4f69f449dec239be24c6be
DN <daniel.neuhoff@googlemail.com>
2023-12-06 21:30:15 +0100

    Day 6 Part 1

--- Day_6/day_6.go
+++ Day_6/day_6.go
@@ -3,13 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Race struct {
+	id        int
+	time      int
+	distance  int
+	waysToWin int
+}
 
 func main() {
 	retVal := playPart1("test0.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 288 {
 		panic("Test 0 failed")
 	}
 	fmt.Println("Test 0 passed")
@@ -35,6 +43,18 @@ func main() {
 	}
 	fmt.Println("Part 1 passed")
 }
+
+func playPart1(fileName string) int {
+	input := readFile(fileName)
+	races := parseForPart1(input)
+	result := 1
+	for _, race := range races {
+		race.determineWinOptions()
+		result *= race.waysToWin
+	}
+	return result
+}
+
 func readFile(fileName string) []string {
 	bytes, _ := os.ReadFile(fileName)
 	input := string(bytes)
@@ -42,16 +62,36 @@ func readFile(fileName string) []string {
 	return splitInput
 }
 
-func playPart1(fileName string) int {
-	lines := readFile(fileName)
-	fmt.Println(lines)
-	return 0
-}
-func parseForPart1(input []string) {
-
+func parseForPart1(input []string) []Race {
+	timeStrings := strings.Fields(input[0])
+	distanceStrings := strings.Fields(input[1])
+	races := make([]Race, 0)
+	for idx := 1; idx < len(timeStrings); idx++ {
+		timeVal, _ := strconv.Atoi(timeStrings[idx])
+		distVal, _ := strconv.Atoi(distanceStrings[idx])
+		races = append(races, Race{time: timeVal, distance: distVal, id: idx})
+	}
+	return races
 }
 
 func playPart2(fileName string) int {
 
 	return 0
+}
+
+func (race *Race) determineWinOptions() {
+	//winDistance <= (time - holdtime)*holdtime <=> time*holdtime - holdtime^2 - winDist> 0 <=> holdtime^2 - time*holdtime + winDist < 0
+	/*zero0 := int(math.Floor(((float64(race.time) - math.Sqrt(float64(race.time*race.time-4*race.distance))) / 2))) + 1
+	zero1 := int(math.Floor(((float64(race.time) + math.Sqrt(float64(race.time*race.time-4*race.distance))) / 2)))
+
+	race.waysToWin = zero1 - zero0
+	fmt.Printf("Race: %d   Ways to win: %d \n", race.id, race.waysToWin)*/
+	//Brute force
+	winOpts := 0
+	for currHold := 0; currHold < race.time; currHold++ {
+		if currHold*(race.time-currHold) > race.distance {
+			winOpts++
+		}
+	}
+	race.waysToWin = winOpts
 }
