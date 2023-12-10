3a91d89ea361813368a9dd0d0a23fc090c576292
DN <daniel.neuhoff@googlemail.com>
2023-12-10 18:07:47 +0100

    Day 10 Part 2

--- day_10/day_10.go
+++ day_10/day_10.go
@@ -36,13 +36,68 @@ func (network *Network) print() {
 	}
 }
 
+func (network *Network) countEnclosed() int {
+	ans := 0
+	for row := 0; row < network.rows; row++ {
+		verticalFound := 0
+		last := "-"
+		for col := 0; col < network.cols; col++ {
+			currNode := network.nodeMap[row][col]
+			if currNode.distance < 0 {
+				currNode.tp = "."
+			}
+			switch currNode.tp {
+			case "|":
+				{
+					verticalFound++
+				}
+			case "-":
+				{
+					continue
+				}
+			case ".":
+				{
+					if verticalFound%2 != 0 {
+						ans++
+						currNode.tp = "I"
+					} else {
+						currNode.tp = " "
+					}
+				}
+
+			case "F", "L", "J", "7":
+				{
+					if last == "-" {
+						last = currNode.tp
+						continue
+					}
+					if (last == "L" && currNode.tp == "7") || (last == "F" && currNode.tp == "J") {
+						verticalFound++
+					}
+					last = "-"
+				}
+			}
+		}
+	}
+	network.print()
+
+	return ans
+}
+
 func playPart0(fileName string) int {
 	lines := readFile(fileName)
-	fmt.Println(lines)
 	network := parseForPart0(lines)
-	network.print()
 	network.init()
 	result := network.traverseFromStart()
+	return result
+}
+
+func playPart1(fileName string) int {
+	lines := readFile(fileName)
+	network := parseForPart0(lines)
+	network.init()
+	network.traverseFromStart()
+	result := network.countEnclosed()
 	return result
 }
 
@@ -56,7 +111,6 @@ func (network *Network) traverseFromStart() int {
 	}
 	for !furthestFound {
 		oldDistance := currNodes[0].distance
-		fmt.Printf("CurrNodes %+v \n", currNodes)
 		for i := 0; i < numPaths; i++ {
 			for _, child := range currNodes[i].children {
 				if child.distance < 0 {
@@ -96,9 +150,11 @@ func (network *Network) init() {
 			}
 		}
 	}
+	startChildOffsets := make([][]int, 0)
+	startNode := network.start
 	for rowOffset := -1; rowOffset <= 1; rowOffset++ {
 		for colOffset := -1; colOffset <= 1; colOffset++ {
-			startNode := network.start
+
 			candidatePos := []int{startNode.pos[0] + rowOffset, startNode.pos[1] + colOffset}
 			if slices.Min(candidatePos) < 0 || candidatePos[0] >= network.rows || candidatePos[1] >= network.cols || (colOffset == 0 && rowOffset == 0) {
 				continue
@@ -106,7 +162,17 @@ func (network *Network) init() {
 			candidateNode := network.nodeMap[startNode.pos[0]+rowOffset][startNode.pos[1]+colOffset]
 			if slices.Contains(candidateNode.children, startNode) {
 				startNode.children = append(startNode.children, candidateNode)
+				startChildOffsets = append(startChildOffsets, []int{rowOffset, colOffset})
 			}
+		}
+	}
+
+	for k, v := range offsets {
+		found := ((slices.Equal(v[0], startChildOffsets[0]) && slices.Equal(v[1], startChildOffsets[1])) ||
+			(slices.Equal(v[1], startChildOffsets[0]) && slices.Equal(v[0], startChildOffsets[1])))
+		if found {
+			startNode.tp = k
+			break
 		}
 	}
 	network.start.distance = 0
@@ -144,11 +210,6 @@ func parseForPart0(input []string) *Network {
 	return ans
 }
 
-func playPart1(fileName string) int {
-
-	return 0
-}
-
 func main() {
 	retVal := playPart0("test0.txt")
 	fmt.Println(retVal)
@@ -171,16 +232,30 @@ func main() {
 	}
 	fmt.Println("Part 0 passed")
 
-	retVal = playPart1("test0.txt")
+	retVal = playPart1("test2.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
-		panic("Test 1 failed")
+	if retVal != 4 {
+		panic("Test 1_1 failed")
 	}
-	fmt.Println("Test 1 passed")
+	fmt.Println("Test 1_1 passed")
+
+	retVal = playPart1("test3.txt")
+	fmt.Println(retVal)
+	if retVal != 8 {
+		panic("Test 1_2 failed")
+	}
+	fmt.Println("Test 1_2 passed")
+
+	retVal = playPart1("test4.txt")
+	fmt.Println(retVal)
+	if retVal != 10 {
+		panic("Test 1_3 failed")
+	}
+	fmt.Println("Test 1_3 passed")
 
 	retVal = playPart1("input.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 567 {
 		panic("Part 1 failed")
 	}
 	fmt.Println("Part 1 passed")
