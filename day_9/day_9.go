0bd52243bdf9f91e43292f4c773b688dcfa602e3
DN <daniel.neuhoff@googlemail.com>
2023-12-10 14:16:37 +0100

    Day 9 Part 1

--- day_9/day_9.go
+++ day_9/day_9.go
@@ -3,20 +3,73 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"slices"
+	"strconv"
 	"strings"
 )
 
-func playPart1(fileName string) int {
-	lines := readFile(fileName)
-	fmt.Println(lines)
-	return 0
+type History struct {
+	sequences [][]int64
 }
 
-func parseForPart1(input []string) {
+func (hist *History) determineSequences() {
+	i := 0
+	for !(slices.Max(hist.sequences[i]) == 0 && slices.Min(hist.sequences[i]) == 0) {
+		var newSequence []int64
+		for idx := 0; idx < len(hist.sequences[i])-1; idx++ {
+			newSequence = append(newSequence, hist.sequences[i][idx+1]-hist.sequences[i][idx])
+		}
+		hist.sequences = append(hist.sequences, newSequence)
+		i++
+	}
+}
+
+func (hist *History) extrapolate() int64 {
+	hist.determineSequences()
+	hist.sequences[len(hist.sequences)-1] = append(hist.sequences[len(hist.sequences)-1], 0)
+	for i := len(hist.sequences) - 2; i >= 0; i-- {
+		hist.sequences[i] = append(hist.sequences[i],
+			hist.sequences[i][len(hist.sequences[i])-1]+hist.sequences[i+1][len(hist.sequences[i+1])-1])
+	}
+	return hist.sequences[0][len(hist.sequences[0])-1]
 
 }
 
-func playPart2(fileName string) int {
+func (hist History) print() {
+	for _, seq := range hist.sequences {
+		fmt.Printf("%+v\n", seq)
+	}
+}
+
+func playPart1(fileName string) int64 {
+	input := readFile(fileName)
+	histories := parseForPart1(input)
+	result := int64(0)
+	for i := 0; i < len(histories); i++ {
+		result += histories[i].extrapolate()
+	}
+	return result
+}
+
+func parseForPart1(input []string) []History {
+	r := regexp.MustCompile(`\-?\d+`)
+	ans := make([]History, 0)
+	for _, line := range input {
+		splits := r.FindAllString(line, -1)
+		currSeq := make([]int64, len(splits))
+		for i := 0; i < len(currSeq); i++ {
+			currVal, _ := strconv.Atoi(splits[i])
+			currSeq[i] = int64(currVal)
+		}
+		ans = append(ans, History{
+			sequences: [][]int64{currSeq},
+		})
+	}
+	return ans
+}
+
+func playPart2(fileName string) int64 {
 
 	return 0
 }
