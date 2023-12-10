8a8f0ab7632a38692edbeac58117deb2e2625a6a
DN <daniel.neuhoff@googlemail.com>
2023-12-10 12:59:02 +0100

    Day 9 init

---
+++ day_9/day_9.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func playPart1(fileName string) int {
+	lines := readFile(fileName)
+	fmt.Println(lines)
+	return 0
+}
+
+func parseForPart1(input []string) {
+
+}
+
+func playPart2(fileName string) int {
+
+	return 0
+}
+
+func main() {
+	retVal := playPart1("test0.txt")
+	fmt.Println(retVal)
+	if retVal != 114 {
+		panic("Test 0 failed")
+	}
+	fmt.Println("Test 0 passed")
+
+	retVal = playPart1("input.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Part 0 failed")
+	}
+	fmt.Println("Part 0 passed")
+
+	retVal = playPart2("test0.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Test 1 failed")
+	}
+	fmt.Println("Test 1 passed")
+
+	retVal = playPart2("input.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Part 1 failed")
+	}
+	fmt.Println("Part 1 passed")
+}
+
+func readFile(fileName string) []string {
+	bytes, _ := os.ReadFile(fileName)
+	input := string(bytes)
+	splitInput := strings.Split(input, "\n")
+	return splitInput
+}
