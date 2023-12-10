8034ae9ed183f4cd7eb82f6fa5ca017bc6adacc5
DN <daniel.neuhoff@googlemail.com>
2023-12-10 14:53:39 +0100

    Day 10 late init

--- day_10/day_10.go
+++ day_10/day_10.go
@@ -6,14 +6,47 @@ import (
 	"strings"
 )
 
+type Node struct {
+	tp       string
+	distance int
+	pos      []int
+	child    *Node
+	isInit   bool
+	id       string
+}
+
+type Network struct {
+	nodeMap [][]*Node
+	grid    [][]rune
+	start   *Node
+}
+
 func playPart0(fileName string) int {
 	lines := readFile(fileName)
 	fmt.Println(lines)
 	return 0
 }
 
-func parseForPart0(input []string) {
+func parseForPart0(input []string) *Network {
 
+	rows := len(input)
+	cols := len(input[0])
+	ans := &Network{
+		nodeMap: make([][]*Node, rows),
+		grid:    make([][]rune, rows),
+	}
+	for row, line := range input {
+		currChars := make([]rune, cols)
+		currNodes := make([]*Node, cols)
+		for col, char := range line {
+			currChars[col] = char
+			currNodes[col] = &Node{
+				tp:     string(char),
+				pos:    []int{row, col},
+				isInit: char == 'S',
+			}
+		}
+	}
 }
 
 func playPart1(fileName string) int {
@@ -25,9 +58,16 @@ func main() {
 	retVal := playPart0("test0.txt")
 	fmt.Println(retVal)
 	if retVal != 0 {
-		panic("Test 0 failed")
+		panic("Test 0_0 failed")
 	}
-	fmt.Println("Test 0 passed")
+	fmt.Println("Test 0_0 passed")
+
+	retVal = playPart0("test1.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Test 0_1 failed")
+	}
+	fmt.Println("Test 0_1 passed")
 
 	retVal = playPart0("input.txt")
 	fmt.Println(retVal)
