8d065be495d4f2312d38fbe318a53d7e20ded556
DN <daniel.neuhoff@googlemail.com>
2023-12-15 00:08:07 +0100

    Day 13 Part 2

--- day_13/day_13.go
+++ day_13/day_13.go
@@ -9,68 +9,71 @@ import (
 type Pattern struct {
 	rows, cols []int
 	nCols      int
+	line       int
 }
 
-func findReflectionLine(axes []int) int {
-	ans := 0
+func findReflectionLine(axes []int, ignore int) int {
+	ans := -1
 	for i := 0; i < len(axes)-1; i++ {
 		maxExtent := i + 1
 		if maxExtent > len(axes)-i-1 {
 			maxExtent = len(axes) - i - 1
 		}
 		match := true
-
 		for offSet := 0; offSet < maxExtent; offSet++ {
 			match = (axes[i-offSet] == axes[i+offSet+1]) && match
-
-			fmt.Println("Col: ", i+1, "MaxExtent: ", maxExtent, "Offset: ", offSet, "Vals: ", axes[i-offSet], axes[i+offSet+1], "Match: ", match)
 		}
-		if match {
+		if match && i+1 != ignore {
 			ans = i + 1
-			fmt.Println("Reflection found at col ", i+1)
-			break
+			return ans
 		}
 	}
 	return ans
 }
 
 func (p *Pattern) fixSmudge() int {
-	initReflectionRow := findReflectionLine(p.rows)
-	initReflectionCol := findReflectionLine(p.cols)
-
+	initReflectionRow := findReflectionLine(p.rows, -1)
+	initReflectionCol := findReflectionLine(p.cols, -1)
+	ans := 0
 	for i := 0; i < len(p.rows); i++ {
 		for j := 0; j < len(p.cols); j++ {
-			p.cols[j] = p.cols[j] ^ (1 << i)
-			p.rows[i] = p.rows[i] ^ (1 << j)
-			newRow := findReflectionLine(p.rows)
-			newCol := findReflectionLine(p.cols)
-			if initReflectionRow != newRow {
-				return newRow
+			p.cols[j] = p.cols[j] ^ (1 << (len(p.rows) - i - 1))
+			p.rows[i] = p.rows[i] ^ (1 << (len(p.cols) - j - 1))
+			newRow := findReflectionLine(p.rows, initReflectionRow)
+			newCol := findReflectionLine(p.cols, initReflectionCol)
+			if initReflectionRow != newRow && newRow > 0 {
+
+				return newRow * 100
 			}
-			if initReflectionCol != newCol {
-				return newCol * 100
+			if initReflectionCol != newCol && newCol > 0 {
+				return newCol
 			}
-			p.cols[j] = p.cols[j] ^ (1 << i)
-			p.rows[i] = p.rows[i] ^ (1 << j)
+			p.cols[j] = p.cols[j] ^ (1 << (len(p.rows) - i - 1))
+			p.rows[i] = p.rows[i] ^ (1 << (len(p.cols) - j - 1))
 		}
 	}
-
-	return -1
-}
-
-func (p *Pattern) getReflectionValue() int {
-	ans := findReflectionLine(p.cols)
-	fmt.Println("Reflection found at col ", ans, "Pattern ", p)
-	ans += findReflectionLine(p.rows) * 100
 
 	return ans
 }
 
+func (p *Pattern) getReflectionValue() int {
+	ans := findReflectionLine(p.cols, -1)
+	if ans > 0 {
+		return ans
+	}
+	ans = findReflectionLine(p.rows, -1) * 100
+	if ans > 0 {
+		return ans
+	}
+	if ans <= 0 {
+		return 0
+	}
+	return -1
+}
+
 func playPart0(fileName string) int {
 	input := readFile(fileName)
-	fmt.Println(input)
 	patterns := parseForPart0(input)
-	fmt.Println(patterns)
 	result := 0
 	for _, p := range patterns {
 		result += p.getReflectionValue()
@@ -80,9 +83,7 @@ func playPart0(fileName string) int {
 
 func playPart1(fileName string) int {
 	input := readFile(fileName)
-	fmt.Println(input)
 	patterns := parseForPart0(input)
-	fmt.Println(patterns)
 	result := 0
 	for _, p := range patterns {
 		result += p.fixSmudge()
@@ -94,10 +95,10 @@ func parseForPart0(input []string) []*Pattern {
 	ans := make([]*Pattern, 0)
 	currPattern := &Pattern{}
 	ans = append(ans, currPattern)
-	for _, line := range input {
+	for idx, line := range input {
 		if len(line) == 0 {
-			fmt.Println("New Pattern")
 			currPattern = &Pattern{}
+			currPattern.line = idx + 1
 			ans = append(ans, currPattern)
 			continue
 		}
@@ -113,11 +114,8 @@ func parseForPart0(input []string) []*Pattern {
 				{
 					currRow = currRow<<1 + 1
 				}
-			default:
-				panic("shouldn't be here")
 			}
 		}
-		fmt.Printf("Line: %s, Bits: %b \n", line, currRow)
 		currPattern.rows = append(currPattern.rows, currRow)
 	}
 	for idx := 0; idx < len(ans); idx++ {
@@ -131,7 +129,6 @@ func parseForPart0(input []string) []*Pattern {
 				}
 				pattern.cols[i] = pattern.cols[i]<<1 + addVal
 			}
-			fmt.Printf("Pattern: %+v\n Bits: %b \n", pattern, pattern.cols[i])
 		}
 	}
 	return ans
@@ -161,7 +158,7 @@ func main() {
 
 	retVal = playPart1("input.txt")
 	fmt.Println(retVal)
-	if retVal != 0 {
+	if retVal != 36771 {
 		panic("Part 1 failed")
 	}
 	fmt.Println("Part 1 passed")
