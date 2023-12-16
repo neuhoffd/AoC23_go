0ad6f46687c91a1b9f3e8b7992cc54c6b7524db5
DN <daniel.neuhoff@googlemail.com>
2023-12-16 16:34:01 +0100

    Day 15 init

---
+++ day_15/day_15.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func playPart0(fileName string) int {
+	input := readFile(fileName)
+	fmt.Println(input)
+	return 0
+}
+
+func parseForPart0(input []string) {
+
+}
+
+func playPart1(fileName string) int {
+
+	return 0
+}
+
+func main() {
+	retVal := playPart0("test0.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Test 0 failed")
+	}
+	fmt.Println("Test 0 passed")
+
+	retVal = playPart0("input.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Part 0 failed")
+	}
+	fmt.Println("Part 0 passed")
+
+	retVal = playPart1("test0.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Test 1 failed")
+	}
+	fmt.Println("Test 1 passed")
+
+	retVal = playPart1("input.txt")
+	fmt.Println(retVal)
+	if retVal != 0 {
+		panic("Part 1 failed")
+	}
+	fmt.Println("Part 1 passed")
+}
+
+func readFile(fileName string) []string {
+	bytes, _ := os.ReadFile(fileName)
+	input := string(bytes)
+	splitInput := strings.Split(input, "\n")
+	for idx := 0; idx < len(splitInput); idx++ {
+		splitInput[idx] = strings.TrimSpace(splitInput[idx])
+	}
+	return splitInput
+}
