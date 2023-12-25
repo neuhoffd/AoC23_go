dd63a78aae65d13e144b1777bd2c73c3f9ae879f
unknown <daniel.neuhoff@googlemail.com>
2023-12-25 15:29:26 +0100

    Day 18 Part 2 Still Ugly

--- day_18_rs/src/main.rs
+++ day_18_rs/src/main.rs
@@ -1,100 +1,124 @@
 use core::fmt;
-use std::{fs, string, vec, iter::once, num};
+use std::{fs, string, vec};
 
 #[derive(Debug)]
 struct Command {
     cmd: char,
-    val: i32,
+    val: i64,
 }
 
-#[derive(Debug)]
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 struct Point {
-    x: i32, y: i32,
+    x: i64,
+    y: i64,
 }
 
 impl std::fmt::Display for Point {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        writeln!(f,"({},{})", self.x, self.y)
+        writeln!(f, "({},{})", self.x, self.y)
     }
 }
 
-fn get_direction(cmd: char) -> (i32, i32) {
-    let dir: (i32, i32);
+fn get_direction(cmd: char) -> (i64, i64) {
     match cmd {
-        'R' => dir = (0, 1),
-        'L' => dir = (0, -1),
-        'U' => dir = (-1, 0),
-        'D' => dir = (1, 0),
+        'R' => (0, 1),
+        'L' => (0, -1),
+        'U' => (-1, 0),
+        'D' => (1, 0),
         _ => {
-            dir = (0, 0);
-            println!("There should be a command here")
+            panic!("There should be a command here")
         }
     }
-    dir
 }
 
 fn main() {
-    let input = fs::read_to_string("C:\\workspace\\AoC23_go\\day_18_rs\\src\\input.txt")
-        .map(string::String::into_boxed_str)
-        .unwrap();
+    let mut res: i64;
 
-    println!("{}", input);
+    assert_eq!(play(".\\test0.txt", true), 62);
+    assert_eq!(play(".\\input.txt", true), 95356);
 
-    let parsed: Vec<Point> = parse(&input);
-
-    println!("{:?}", parsed);
-
-    print_points(&parsed);
-    println!("{}", calc_area(&parsed));
+    assert_eq!(play(".\\test0.txt", false), 952408144115);
+    assert_eq!(play(".\\input.txt", false), 92291468914147);
 }
 
-fn calc_area(points: &Vec<Point>) -> i32 {
-    let mut area :i32= 0;
+fn play(path: &str, part1: bool) -> i64 {
+    let parsed: Vec<Point> = parse(
+        &fs::read_to_string(path)
+            .map(string::String::into_boxed_str)
+            .unwrap(),
+        part1,
+    );
+
+    let area = area(&parsed);
+    println!(
+        "Part {}: \nPath: {} \nArea: {}",
+        if part1 { 1 } else { 2 },
+        path,
+        area
+    );
+    area
+}
+
+fn area(points: &Vec<Point>) -> i64 {
+    let mut area: i64 = 0;
     let n: usize = points.len();
-    let mut j: usize = n-1;
-    let mut perimeter: i32 = 0;
+    let mut j: usize = n - 1;
+    let mut perimeter: i64 = 0;
 
     for i in 0..n {
-        let term1 = points[j].x + points[i].x;
-        let term2 = points[j].y - points[i].y;
-        println!("Area {}, i {}, j {}, t1 {}, t2 {}, p {}", area / 2, i, j, term1, term2, perimeter);
-        
-        area += (points[j].x + points[i].x) * (points[j].y - points[i].y);       
-        perimeter += (points[j].x - points[i].x).abs() + term2.abs();
-        j = i; 
+        area += (points[j].x + points[i].x) * (points[j].y - points[i].y);
+        perimeter += (points[j].x - points[i].x).abs() + (points[j].y - points[i].y).abs();
+        j = i;
     }
     area / 2 + perimeter / 2 + 1
 }
 
-fn print_points(points: &Vec<Point>) {
-    println!("{}",points.iter().map(|p| p.to_string()).collect::<Vec<String>>().join(""))
-}
-
-fn parse(input: &str) -> Vec<Point> {
-    let mut pos : Point = Point { x: 0, y: 0 };
-
-    let cmds =     
+fn parse(input: &str, part1: bool) -> Vec<Point> {
+    let pos: Point = Point { x: 0, y: 0 };
+    let cmds = if part1 {
         input
-        .lines()
-        .map(|line| -> Command {
-            let mut splits = line.split_whitespace();
-            Command {
-                cmd: splits.next().unwrap().chars().next().unwrap(),
-                val: splits.next().unwrap().parse().unwrap(),
-            }
-        })
-        .collect::<Vec<Command>>();
+            .lines()
+            .map(|line| -> Command {
+                let mut splits = line.split_whitespace();
+                Command {
+                    cmd: splits.next().unwrap().chars().next().unwrap(),
+                    val: splits.next().unwrap().parse().unwrap(),
+                }
+            })
+            .collect::<Vec<Command>>()
+    } else {
+        input
+            .lines()
+            .map(|line| -> Command {
+                let splits = line.split_whitespace();
+                let hex = splits.skip(2).next().unwrap();
+                Command {
+                    val: i64::from_str_radix(&hex[2..7], 16).unwrap(),
+                    cmd: match &hex[hex.len() - 2..hex.len() - 1] {
+                        "0" => 'R',
+                        "1" => 'D',
+                        "2" => 'L',
+                        "3" => 'U',
+                        _ => panic!("This shouldn't happen"),
+                    },
+                }
+            })
+            .collect::<Vec<Command>>()
+    };
 
-    let mut ans : Vec<Point> = vec![Point{x:0, y:0}];
-    ans.append(&mut cmds.iter()
-    .scan(pos,  |mut pos: &mut Point, cmd: &Command| -> Option<Point> {
-        let dir: (i32, i32);
-        dir = get_direction(cmd.cmd);
+    let mut ans: Vec<Point> = vec![Point { x: 0, y: 0 }];
+    ans.append(
+        &mut cmds
+            .iter()
+            .scan(pos, |pos: &mut Point, cmd: &Command| -> Option<Point> {
+                let dir: (i64, i64);
+                dir = get_direction(cmd.cmd);
 
-        pos.x = pos.x + dir.0 * cmd.val;
-        pos.y = pos.y + dir.1 * cmd.val;
-        Some(pos.clone())
-    }).collect::<Vec<Point>>());
-    ans.split_last().unwrap().1.to_vec()  
+                pos.x = pos.x + dir.0 * cmd.val;
+                pos.y = pos.y + dir.1 * cmd.val;
+                Some(pos.clone())
+            })
+            .collect::<Vec<Point>>(),
+    );
+    ans.split_last().unwrap().1.to_vec()
 }
